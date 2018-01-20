b8fc1a2968f496d0de01f094a452bbf10c9a8c2f
Ming-FlyingWood <mingfei.wu@outlook.com>
2018-01-20 14:44:02 +0800

    Practice
    Practice

---
+++ headfirst/gumball/mightygumball.js
@@ -0,0 +1,19 @@
+﻿/* 文档放服务器上，输入HTML文件URL运行*/
+window.onload = function() {
+	var url = "http://localhost/headfirst/gumball/sales.json";
+	var request = new XMLHttpRequest();
+	request.open("GET", url);
+	request.onload = function() {
+		if (request.status == 200) {
+			updateSales(request.responseText);
+		}
+	};
+	request.send(null);
+}
+function updateSales(responseText) {
+	var salesDiv = document.getElementById("sales");
+	//salesDiv.innerHTML = responseText;
+	var sales = JSON.parse(responseText);//把JSON串转换为对象
+	
+}
+	
